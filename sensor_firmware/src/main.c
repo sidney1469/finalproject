c35e0e54bae2ad43c68f62f537bcc5bc5eccca02
47429914 <x.akison@student.uq.edu.au>
2026-05-18 13:29:46 +1000

    Created serial helper and test_receive

--- sensor_firmware/src/main.c
+++ sensor_firmware/src/main.c
@@ -3,10 +3,30 @@
 #include <stdio.h>
 #include "ble_transfer.h"
 #include "gps_driver.h"
+#include "serial.h"
+
+// int main(void)
+// {
+//     int err = serial_init();
+//     if (err) {
+//         printk("serial_init failed: %d\n", err);
+//         return 0;
+//     }
+
+//     while (1) {
+//         char *line = serial_read_line();
+//         if (line) {
+//             send_over_nus(line);
+//             printk("USB->NUS: %s\n", line);
+//         }
+
+//         k_msleep(5);
+//     }
+//     return 0;
+// }
 
 int main(void)
 {
-    /* Give GPS thread time to get first fix */
     k_sleep(K_SECONDS(5));
 
     while (1) {
