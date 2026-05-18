7d145ebf3b7cdd424a4ef37dba448f2e2173d005
47429914 <x.akison@student.uq.edu.au>
2026-05-18 15:00:57 +1000

    sensor node configured

--- sensor_firmware/src/main.c
+++ sensor_firmware/src/main.c
@@ -5,48 +5,52 @@
 #include "gps_driver.h"
 #include "serial.h"
 
-// int main(void)
-// {
-//     int err = serial_init();
-//     if (err) {
-//         printk("serial_init failed: %d\n", err);
-//         return 0;
-//     }
-
-//     while (1) {
-//         char *line = serial_read_line();
-//         if (line) {
-//             send_over_nus(line);
-//             printk("USB->NUS: %s\n", line);
-//         }
-
-//         k_msleep(5);
-//     }
-//     return 0;
-// }
-
 int main(void)
 {
-    k_sleep(K_SECONDS(5));
-
-    while (1) {
-        gps_location_t loc = get_location();
-
-        if (loc.valid) {
-            char msg[200];
-            snprintf(msg, sizeof(msg),
-                     "{\"lat\":%.6f,\"lon\":%.6f,\"alt\":%.2f}",
-                     loc.latitude, loc.longitude, loc.altitude_m);
-            
-            send_over_nus(msg);
-            printk("Sent: %s\n", msg);
-        } else {
-            printk("Waiting for GPS fix...\n");
-            send_over_nus("TEST MESSAGE since GPS isn't working");
-        }
-
-        k_sleep(K_SECONDS(2));  /* Send every 10 seconds */
+    int err = serial_init();
+    if (err) {
+        printk("serial_init failed: %d\n", err);
+        return 0;
     }
 
+    if (err) {
+        printk("bluetooth_init failed: %d\n", err);
+        return 0;
+    }
+
+    while (1) {
+        char *line = serial_read_line();
+        if (line) {
+            send_over_nus(line);
+            printk("USB->NUS: %s\n", line);
+        }
+        k_msleep(5);
+    }
     return 0;
 }
+
+// int main(void)
+// {
+//     k_sleep(K_SECONDS(5));
+
+//     while (1) {
+//         gps_location_t loc = get_location();
+
+//         if (loc.valid) {
+//             char msg[200];
+//             snprintf(msg, sizeof(msg),
+//                      "{\"lat\":%.6f,\"lon\":%.6f,\"alt\":%.2f}",
+//                      loc.latitude, loc.longitude, loc.altitude_m);
+            
+//             send_over_nus(msg);
+//             printk("Sent: %s\n", msg);
+//         } else {
+//             printk("Waiting for GPS fix...\n");
+//             send_over_nus("TEST MESSAGE since GPS isn't working");
+//         }
+
+//         k_sleep(K_SECONDS(2));  /* Send every 10 seconds */
+//     }
+
+//     return 0;
+// }
