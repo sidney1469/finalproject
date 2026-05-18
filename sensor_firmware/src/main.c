2246b89d192680132242127092e455e2df50f933
47429914 <x.akison@student.uq.edu.au>
2026-05-18 11:48:58 +1000

    main.c test for both drivers

--- sensor_firmware/src/main.c
+++ sensor_firmware/src/main.c
@@ -1,10 +1,31 @@
 #include <zephyr/sys/printk.h>
+#include <zephyr/kernel.h>
+#include <stdio.h>
 #include "ble_transfer.h"
 #include "gps_driver.h"
 
 int main(void)
 {
-    char* msg = "TEST";
-    send_over_nus(msg);
+    /* Give GPS thread time to get first fix */
+    k_sleep(K_SECONDS(5));
+
+    while (1) {
+        gps_location_t loc = get_location();
+
+        if (loc.valid) {
+            char msg[200];
+            snprintf(msg, sizeof(msg),
+                     "{\"lat\":%.6f,\"lon\":%.6f,\"alt\":%.2f}",
+                     loc.latitude, loc.longitude, loc.altitude_m);
+            
+            send_over_nus(msg);
+            printk("Sent: %s\n", msg);
+        } else {
+            printk("Waiting for GPS fix...\n");
+        }
+
+        k_sleep(K_SECONDS(10));  /* Send every 10 seconds */
+    }
+
     return 0;
 }
